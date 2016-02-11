cd4ff3460a1dd8c06d9fa4cce00e3df9b5f9e82a
Devin Leonhart <devinleonhart@gmail.com>
2016-02-10 18:48:55 -0800

    Added Yeoman Scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name katboxBazaarApp
+ * @description
+ * # katboxBazaarApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('katboxBazaarApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name katboxBazaarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the katboxBazaarApp
+ */
+angular.module('katboxBazaarApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
