4f9d22e023bfb9af7fb08b427bd6e602a7758c64
Devin Leonhart <devinleonhart@gmail.com>
2016-02-17 17:55:15 -0800

    Added ui-router and used bootsrap grid system

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,5 +8,16 @@
  *
  * Main module of the application.
  */
-angular
-  .module('katboxBazaarApp', []);
+angular.module('katboxBazaarApp', ['ui.router']);
+
+angular.module('katboxBazaarApp').config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state('main', {
+    url: '/',
+    controller: 'MainCtrl',
+    templateUrl: 'views/main.html'
+  });
+
+  $urlRouterProvider.otherwise('/');
+
+});
